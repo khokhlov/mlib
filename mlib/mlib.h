00be12183e1af14c9c2ecd1731257c322e54adfd
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2012-10-08 13:31:50 +0400

    Added 1-4 dimension vectors

---
+++ mlib/mlib.h
@@ -0,0 +1,12 @@
+/*
+ * Author: Nikolay Khokhlov <k_h@inbox.ru>, (C) 2012
+ */
+
+#ifndef MLIB_H
+#define MLIB_H
+
+#include "mlib_core.h"
+#include "mlib_vector.h"
+#include "mlib_matrix.h"
+
+#endif // MLIB_H
--- mlib/mlib_vector.h
+++ mlib/mlib_vector.h
@@ -6,8 +6,21 @@
 #define MLIB_VECTOR_H
 
 #include "mlib_core.h"
+#include "mlib_matrix.h"
+
+#define mlib_vector1(T) mlib::matrix<mlib::basic_vector<mlib::vector1_t<T> > >
+#define mlib_vector2(T) mlib::matrix<mlib::basic_vector<mlib::vector2_t<T> > >
+#define mlib_vector3(T) mlib::matrix<mlib::basic_vector<mlib::vector3_t<T> > >
+#define mlib_vector4(T) mlib::matrix<mlib::basic_vector<mlib::vector4_t<T> > >
 
 namespace mlib {
+	enum cart_dir {
+		X = 0,
+		Y,
+		Z,
+		All_DIRS
+	};
+
 	template <class base>
 	struct basic_vector : public base {
 		typedef typename base::type T;
@@ -15,6 +28,27 @@ namespace mlib {
 		enum { _rows = _size, };
 		enum { _cols = 1, };
 		basic_vector() : base() {}
+	};
+
+	template <class T>
+	struct vector1_t {
+		typedef T type;
+		type x;
+	};
+	template <class T>
+	struct vector2_t {
+		typedef T type;
+		type x, y;
+	};
+	template <class T>
+	struct vector3_t {
+		typedef T type;
+		type x, y, z;
+	};
+	template <class T>
+	struct vector4_t {
+		typedef T type;
+		type x, y, z, w;
 	};
 }; // mlib
 
